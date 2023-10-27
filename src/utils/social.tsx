ae3f2fc35401f6c84a85f69c10059986be462fee
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-27 00:19:04 -0300

    feat: Add socialMedias array

---
+++ src/utils/social.tsx
@@ -0,0 +1,23 @@
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa"
+import { FaSquareXTwitter } from "react-icons/fa6"
+
+export const socialMedias: Social[] = [
+  {
+    order: 1,
+    name: "Facebook",
+    path: "https://www.facebook.com/pipocaagil",
+    icon: FaFacebookSquare
+  },
+  {
+    order: 2,
+    name: "Instagram",
+    path: "https://www.instagram.com/pipocaagil",
+    icon: FaInstagramSquare
+  },
+  {
+    order: 3,
+    name: "X",
+    path: "https://twitter.com/pipoca_agil",
+    icon: FaSquareXTwitter
+  }
+]
