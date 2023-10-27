182fe33c80b160eb13aa1c03bd7927c454c77494
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-27 00:40:38 -0300

    feat: Add pages

---
+++ src/app/PopcornClub/page.tsx
@@ -0,0 +1,9 @@
+import { HomeContainer } from "@/components/home/HomeContainer"
+
+export default function PopcornClub() {
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <HomeContainer />
+    </main>
+  )
+}
