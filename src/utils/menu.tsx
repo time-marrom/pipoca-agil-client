ccdfe4d729526b5691df81522a96c9935cc2ab8e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-27 00:19:54 -0300

    feat: Add menuOptions array

---
+++ src/utils/menu.tsx
@@ -0,0 +1,38 @@
+export const menuOptions: MenuOption[] = [
+  {
+    name: "Início",
+    path: "/",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 1
+  },
+  {
+    name: "Simulação",
+    path: "/simulacao",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 2
+  },
+  {
+    name: "Podcast",
+    path: "/podcast",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 3
+  },
+  {
+    name: "Trilha Ágil",
+    path: "/trilha-agil",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 4
+  },
+  {
+    name: "Pipoca Indica",
+    path: "/pipoca-indica",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 5
+  },
+  {
+    name: "Contato",
+    path: "/contato",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
+    order: 6
+  }
+]
