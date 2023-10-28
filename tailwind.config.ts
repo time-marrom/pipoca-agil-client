0269c7d3e7736e18eeb5c41c2c25eaeb1ccadd21
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-28 16:10:28 -0300

    chore: Revise color labels for improved organization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,33 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--font-sans)"],
         title: ["var(--font-title)"]
+      },
+      colors: {
+        primary: {
+          light: "#FFBA4C",
+          base: "#FF9C00",
+          dark: "#CA7B00"
+        },
+        secondary: {
+          light: "#9243C0",
+          base: "#7401B7",
+          dark: "#4C0178"
+        },
+        grayscale: {
+          black: "#252525",
+          darker: "#3A3A3A",
+          darkest: "#727272"
+        },
+        white: {
+          base: "#F6F6F6",
+          light: "#DEDEDE",
+          lighter: "#C3C3C3"
+        },
+        status: {
+          warning: "#FFF500",
+          success: "#04C500",
+          error: "#E70000"
+        }
       }
     }
   },
