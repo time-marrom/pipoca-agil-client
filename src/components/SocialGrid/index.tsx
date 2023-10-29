d5fce537c1f63bd177970323000667a4a2e08064
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-10-29 12:38:53 -0300

    feat: Add LinkedIn in SocailGrid component

--- src/components/SocialGrid/index.tsx
+++ src/components/SocialGrid/index.tsx
@@ -5,11 +5,11 @@ export function SocialGrid(): JSX.Element {
   const orderedSocialMedias = socialMedias.sort((a, b) => a.order - b.order)
 
   return (
-    <ul className="h-full w-32 grid grid-cols-3 grid-rows-1 gap-1">
+    <ul className="max-h-max w-48 grid grid-cols-4 grid-rows-1 gap-1">
       {orderedSocialMedias.map(({ name, path, icon: Icon }) => (
-        <li key={name} className="flex items-center text-center w-8 h-full p-1">
+        <li key={name} className="flex items-center text-center w-8 max-h-max p-1">
           <Link href={path} className="flex items-center text-center rounded-sm">
-            <Icon className="w-6 h-6 font-bold text-[#7401B7] hover:text-[#4C0178] cursor-pointer transition duration-300" />
+            <Icon className="w-6 h-6 font-bold cursor-pointer transition duration-300 text-secondary-base hover:text-secondary-dark" />
           </Link>
         </li>
       ))}
