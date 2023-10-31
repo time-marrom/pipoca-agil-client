b92b7d8c45ef8b17453937822b50b3877ad5a176
Pliii2020 <pedrooliveira2702@gmail.com>
2023-10-31 09:24:09 -0300

    Novo componente

---
+++ src/components/home/PartnersGrid/index.tsx
@@ -0,0 +1,14 @@
+
+
+
+export function PartnersGrid() {
+
+    return (
+        <ul>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut distinctio placeat suscipit omnis incidunt magnam debitis, eligendi, odit cumque dolorum quibusdam voluptates cupiditate dolor laudantium nisi. Vel in eum ullam?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam dolores tempore, atque cumque minima nulla ex ad. Maiores consectetur aliquam quisquam quidem corrupti tempore minus ad natus, obcaecati deleniti.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus corrupti officiis adipisci nisi odio autem, doloremque eaque incidunt quis, suscipit rem dicta natus illo reprehenderit laborum officia provident. Eaque, earum!</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque dolorem eum ipsum deleniti blanditiis iste eius quam modi. Porro magni accusamus at pariatur ad odit vero! Cum sed officiis ipsum.</li>
+        </ul>
+    )
+}
