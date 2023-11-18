04f9344c9d04b1adba082dbbcb1d3a170227825a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-11-17 22:51:18 -0300

    feat: create RootProviders provider.

---
+++ src/providers/rootProvider.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { GlobalContextProvider } from "../contexts/GlobalContext"
+
+type RootProvidersProps = {
+  children: React.ReactNode
+}
+
+export function RootProviders({ children }: RootProvidersProps) {
+  return <GlobalContextProvider>{children}</GlobalContextProvider>
+}
