39e651c03075137d055de749b1cf80f9bf1d24bc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-11-17 23:01:35 -0300

    chore: instal shadcn lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,9 +38,30 @@ const config: Config = {
           success: "#04C500",
           error: "#E70000"
         }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" }
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" }
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
+      }
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px"
       }
     }
   },
-  plugins: []
+  plugins: [require("tailwindcss-animate")]
 }
 export default config
