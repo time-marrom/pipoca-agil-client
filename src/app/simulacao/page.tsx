0737776ccbdc2650b80fc835eb35d19f892ccb9d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-11-19 22:47:11 -0300

    feat: add SimulationFAQ component

--- src/app/simulacao/page.tsx
+++ src/app/simulacao/page.tsx
@@ -1,10 +1,10 @@
-import { MaintenanceContent } from "@/components/MaintenanceContent"
-import { PageContainer } from "@/components/PageContainer"
+import { PageContainer } from "@/components/PageContainer";
+import { SimulationFAQ } from "@/components/simulation/SimulationFAQ";
 
 export default function SimulationPage() {
   return (
     <PageContainer>
-      <MaintenanceContent pageName="Simulação" />
+      <SimulationFAQ />
     </PageContainer>
-  )
+  );
 }
