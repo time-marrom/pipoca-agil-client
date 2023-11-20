11980ea873a516e5b0b91b812f05baa080f60a9f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-11-19 23:27:45 -0300

    feat: add SimulationTestimony component.

---
+++ src/components/simulation/SimulationTestimony/index.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+export type SimulationTestimonyArray = {
+  id: number;
+  name: string;
+  testimony: string;
+  image: string;
+};
+
+const mockArray = [
+  {
+    id: 2,
+    name: "Maria",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://via.placeholder.com/150/771796",
+  },
+  {
+    id: 3,
+    name: "Ana",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://via.placeholder.com/150/24f355",
+  },
+  {
+    id: 5,
+    name: "Lucas",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://via.placeholder.com/150/24f355",
+  },
+  {
+    id: 6,
+    name: "Rafaela",
+    testimony:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://via.placeholder.com/150/771796",
+  },
+];
+
+export function SimulationTestimony(): JSX.Element {
+  return (
+    <div className="h-full w-full py-4 space-y-8 flex flex-col justify-start items-center bg-theme-white-base">
+      <h3 className="font-sans font-medium text-lg">Vale a pena?</h3>
+      <div className="w-full flex flex-row gap-2 justify-center items-center">
+        {mockArray.map((item) => (
+          <div
+            key={item.id}
+            className="w-1/6 min-h-[250px] flex flex-col justify-center items-center text-center space-y-2 p-4 rounded-md shadow-md bg-theme-white-light"
+          >
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={150}
+              height={150}
+              priority
+              className="w-20 h-20 rounded-full object-cover object-center"
+            />
+            <p className="font-sans font-semibold text-base">{item.name}</p>
+            <p className="font-sans font-normal text-base italic">
+              {item.testimony}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
