379bc678c6d641dffd0b59f7647ce5925c9ddd1e
PLiii2020 <pedrooliveira2702@gmail.com>
2023-11-21 11:31:22 -0300

    Simulation description

---
+++ src/components/home/SimulationDescription/index.tsx
@@ -0,0 +1,10 @@
+export function SimulationDescription() {
+    return (
+        <div className="flex flex-column justify-start">
+      <h3 className="text-grayscale-black text-2xl font-bold font-title mb-4 "> O que é ?</h3>
+     <p className="w-400 text-grayscale-darkest text-start font-sans mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque fugit, accusamus ab, numquam iste magni nisi, corporis neque commodi eveniet non reprehenderit accusantium excepturi aspernatur a quibusdam id optio.</p>
+        </div>
+    )
+
+
+}
