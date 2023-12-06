cd5851c5ca415906e278cba7b0d459053f395a58
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-06 02:46:56 -0300

    refactor: accessibility and responsiveness adjustments

--- src/components/registration/RegistrationPanel/Steps.tsx
+++ src/components/registration/RegistrationPanel/Steps.tsx
@@ -10,27 +10,27 @@ export type StepsProps = {
 
 export function Steps({ currentStep }: StepsProps) {
   return (
-    <div className="w-full flex flex-row justify-center items-center text-center mx-auto mb-2 gap-4">
+    <div className="w-full flex flex-row justify-center items-center text-center mx-auto mb-2 gap-1 md:gap-6">
       <div className="flex flex-col items-center justify-center text-center p-1">
         <MdOutlineAssignment
           data-current-step={currentStep}
           className="text-lg mb-1 h-6 w-6 fill-current data-[current-step='0']:text-theme-secondary-base data-[current-step='1']:text-theme-secondary-base data-[current-step='2']:text-theme-secondary-base"
         />
-        <p className="font-medium text-lg font-title">Termo de Consentimento</p>
+        <p className="font-medium text-sm md:text-lg font-title">Termo de Consentimento</p>
       </div>
       <div className="flex flex-col items-center justify-center text-center p-1">
         <AiOutlineUser
           data-current-step={currentStep}
           className="text-lg mb-1 h-6 w-6 fill-current data-[current-step='1']:text-theme-secondary-base data-[current-step='2']:text-theme-secondary-base"
         />
-        <p className="font-medium text-lg font-title">Preenchimento de Dados</p>
+        <p className="font-medium text-sm md:text-lg font-title">Preenchimento de Dados</p>
       </div>
       <div className="flex flex-col items-center justify-center text-center p-1">
         <GiConfirmed
           data-current-step={currentStep}
           className="text-lg mb-1 h-6 w-6 fill-current data-[current-step='2']:text-theme-secondary-base"
         />
-        <p className="font-medium text-lg font-title">Confirmação</p>
+        <p className="font-medium text-sm md:text-lg font-title">Confirmação</p>
       </div>
     </div>
   );
