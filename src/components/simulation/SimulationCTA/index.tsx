f8c8abe5570ae81ac692fc36cdfbaa5be9f1fd72
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-06 02:45:21 -0300

    refactor: accessibility and responsiveness adjustments

--- src/components/simulation/SimulationCTA/index.tsx
+++ src/components/simulation/SimulationCTA/index.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export function SimulationCTA() {
   return (
-    <div className="w-full h-[400px] p-8 flex flex-row justify-between items-center bg-theme-white-lighter">
-      <div className="w-1/2">
-        <ul className="list-none w-10/12 space-y-5">
+    <div className="w-full h-full p-4 md:p-20 flex flex-col-reverse lg:flex-row justify-between items-center bg-theme-white-lighter">
+      <div className="w-full lg:w-1/2">
+        <ul className="list-none w-10/12 space-y-5 my-5">
           {mockArray.map(({ testimony, id, image, name }) => (
             <li
               key={id}
@@ -25,7 +25,7 @@ export function SimulationCTA() {
           ))}
         </ul>
       </div>
-      <div className="w-1/2 flex flex-col justify-start items-start space-y-4">
+      <div className="w-full lg:w-1/2 flex flex-col justify-start items-start space-y-5 my-5">
         <h3 className="font-sans font-medium text-lg">Para quem é?</h3>
         <p className="font-sans font-normal text-base">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
