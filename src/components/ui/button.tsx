b394283a2694b7809b7cbc6a91e57b29e64f36ee
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-06 02:46:31 -0300

    refactor: accessibility and responsiveness adjustments

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,16 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+          "bg-theme-secondary-base text-slate-50 hover:bg-theme-secondary-base/90 dark:bg-slate-50",
         destructive:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-slate-50 hover:bg-red-500/90",
         outline:
-          "border border-slate-200 bg-theme-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+          "border border-slate-200 bg-theme-white hover:bg-slate-100 hover:text-theme-secondary-base",
         secondary:
-          "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+          "bg-slate-100 text-theme-secondary-base hover:bg-slate-100/80",
         ghost:
-          "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
-        link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
+          "hover:bg-slate-100 hover:text-theme-secondary-base",
+        link: "text-theme-secondary-base underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
