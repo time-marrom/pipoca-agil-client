c24d96a80669418af128862c96d692442f2bc4c2
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-06 02:44:27 -0300

    refactor: accessibility and responsiveness adjustments

--- src/components/home/PopcornClubCTA/index.tsx
+++ src/components/home/PopcornClubCTA/index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export function PopcornClubCTA(): JSX.Element {
   return (
-    <div className="w-full h-80 flex flex-col items-start justify-center text-start p-12">
+    <div className="w-full h-80 p-4 md:p-20 flex flex-col items-start justify-center text-start">
       <div className="w-full h-full rounded-lg">
         <h3 className="text-grayscale-black text-2xl font-bold font-title mb-4">
           Clube pipoca
@@ -13,12 +14,11 @@ export function PopcornClubCTA(): JSX.Element {
           nemo error, sed tenetur ad blanditiis deserunt deleniti repudiandae
           saepe praesentium.
         </p>
-        <Link
-          href="/pipoca-indica"
-          className="h-10 max-w-max my-1 px-4 py-2 text-base font-medium font-sans rounded-md text-center text-theme-white-base cursor-pointer transition duration-300 bg-theme-secondary-base hover:bg-theme-secondary-dark"
-        >
-          Ver mais
-        </Link>
+        <Button variant="default" size="sm" asChild>
+          <Link href="/pipoca-indica" className="font-sans text-base">
+            Ver mais
+          </Link>
+        </Button>
       </div>
     </div>
   );
