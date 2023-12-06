d340d284c5bd902fc73867a73253a2ef7ea9fdec
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-06 02:43:18 -0300

    refactor: accessibility and responsiveness adjustments

--- src/components/home/AgileTrailCTA/index.tsx
+++ src/components/home/AgileTrailCTA/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -31,8 +32,8 @@ const mock = [
 
 export function AgileTrailCTA(): JSX.Element {
   return (
-    <div className="w-full h-max flex flex-row items-start justify-between text-start p-12 gap-2 bg-theme-white-light">
-      <div className="w-1/2 h-full flex flex-col items-start justify-center text-start gap-4">
+    <div className="w-full h-max p-4 md:p-20 flex flex-col-reverse md:flex-row items-start justify-between text-start gap-2 bg-theme-white-light">
+      <div className="w-full md:w-1/2 h-full flex flex-col items-start justify-center text-start gap-4">
         <h4 className="text-grayscale-black text-xl font-bold font-title mb-4">
           Artigos recentes
         </h4>
@@ -40,7 +41,7 @@ export function AgileTrailCTA(): JSX.Element {
           {mock.map((item) => (
             <li
               key={item.id}
-              className="flex flex-row items-center justify-start gap-4 bg-theme-white-lighter p-4 rounded-md shadow-md h-28 w-96"
+              className="flex flex-col md:flex-row items-center justify-start gap-4 bg-theme-white-lighter p-4 rounded-md shadow-md text-center md:text-start h-full md:h-28 w-full max-w-sm"
             >
               <Image
                 src={item.thumbnailUrl}
@@ -56,14 +57,13 @@ export function AgileTrailCTA(): JSX.Element {
             </li>
           ))}
         </ul>
-        <Link
-          href="/trilha-agil"
-          className="h-10 max-w-max my-1 px-4 py-2 text-base font-medium font-sans rounded-md text-center text-theme-white-base cursor-pointer transition duration-300 bg-theme-secondary-base hover:bg-theme-secondary-dark"
-        >
-          Ver todos
-        </Link>
+        <Button variant="default" size="sm" asChild>
+          <Link href="/trilha-agil" className="font-sans text-base">
+            Ver todos
+          </Link>
+        </Button>
       </div>
-      <div className="w-1/2 h-full flex flex-col items-start justify-center text-start">
+      <div className="w-full md:w-1/2 h-full flex flex-col items-start justify-center text-start">
         <h3 className="text-grayscale-black text-2xl font-bold font-title mb-4">
           Trilha Ágil
         </h3>
@@ -73,12 +73,11 @@ export function AgileTrailCTA(): JSX.Element {
           nemo error, sed tenetur ad blanditiis deserunt deleniti repudiandae
           saepe praesentium.
         </p>
-        <Link
-          href="/trilha-agil"
-          className="h-10 max-w-max my-1 px-4 py-2 text-base font-medium font-sans rounded-md text-center text-theme-white-base cursor-pointer transition duration-300 bg-theme-secondary-base hover:bg-theme-secondary-dark"
-        >
-          Ver todas as trilhas
-        </Link>
+        <Button variant="default" size="sm" asChild>
+          <Link href="/trilha-agil" className="font-sans text-base">
+            Ver todas as trilhas
+          </Link>
+        </Button>
       </div>
     </div>
   );
