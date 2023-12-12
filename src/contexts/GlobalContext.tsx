7c2e83662e0d58df2adb526bad2f092f23cdfab3
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-12-11 23:57:15 -0300

    feat: add role in global context

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -3,6 +3,8 @@ import * as React from "react"
 type GlobalContextProps = {
   isAcceptedTerms: boolean
   setIsAcceptedTerms: React.Dispatch<React.SetStateAction<boolean>>
+  role: string
+  setRole: React.Dispatch<React.SetStateAction<string>>
 }
 
 type GlobalContextProviderProps = {
@@ -11,20 +13,25 @@ type GlobalContextProviderProps = {
 
 const DEFAULT_VALUE = {
   isAcceptedTerms: false,
-  setIsAcceptedTerms: () => {}
+  setIsAcceptedTerms: () => {},
+  role: "",
+  setRole: () => {}
 }
 
 export const GlobalContext = React.createContext<GlobalContextProps>(DEFAULT_VALUE)
 
 export function GlobalContextProvider({ children }: GlobalContextProviderProps) {
   const [isAcceptedTerms, setIsAcceptedTerms] = React.useState<boolean>(false)
+  const [role, setRole] = React.useState<string>("VOLUNTÁRIO")
 
   const memoizedValue = React.useMemo(
     () => ({
       isAcceptedTerms,
-      setIsAcceptedTerms
+      setIsAcceptedTerms,
+      role,
+      setRole
     }),
-    [isAcceptedTerms]
+    [isAcceptedTerms, setIsAcceptedTerms, role, setRole]
   )
 
   return <GlobalContext.Provider value={memoizedValue}>{children}</GlobalContext.Provider>
