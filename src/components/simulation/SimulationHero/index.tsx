dceead59a53a6d5dc17e069f1f84b14c20ea507d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 22:52:41 -0300

    refactor: Refactor SimulationHero component to use client-side rendering

--- src/components/simulation/SimulationHero/index.tsx
+++ src/components/simulation/SimulationHero/index.tsx
@@ -1,20 +1,26 @@
-export function SimulationHero() {
+"use client";
+
+import { getSanitySimulationContent } from "@/services/axios";
+import { useQuery } from "@tanstack/react-query";
+
+interface SimulationHeroProps {
+  content: SimulationContent;
+}
+
+export function SimulationHero({ content }: SimulationHeroProps) {
+  const { data } = useQuery({
+    queryKey: ["simulation"],
+    queryFn: getSanitySimulationContent,
+    initialData: content,
+  });
+
   return (
     <div className="w-full h-full p-4 md:p-20 flex flex-col lg:flex-row justify-between items-center bg-theme-white-light">
       <h3 className="text-theme-grayscale-black text-2xl font-bold font-title mb-4">
-        Simulação de Projetos Ágeis
+        {data.heroTitle}
       </h3>
       <p className="w-full lg:w-1/2 text-center md:text-start font-sans mb-6 text-theme-grayscale-darkest">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt sed
-        magnam vel ex earum, aliquam voluptas asperiores dolores esse
-        exercitationem sapiente quae et voluptatem rem, nisi suscipit enim
-        officiis eum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Maxime ratione sed, ut asperiores repellat maiores delectus ducimus
-        eligendi praesentium aperiam eveniet suscipit neque, expedita ipsa
-        tempore, possimus quidem provident voluptatum! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Enim eius voluptate perspiciatis aut
-        pariatur officiis, maiores molestias a assumenda expedita tempora!
-        Molestias quae rem veniam deleniti repellendus unde doloribus beatae?
+        {data.heroSubtitle}
       </p>
     </div>
   );
