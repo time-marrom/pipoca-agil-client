869cc32c657298a2533adb31520f128525417ef2
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 22:45:30 -0300

    refactor: Update types in globals.d.ts

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,62 +1,69 @@
 type RootLayoutProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 type SanityQueries = {
-  maintenance: string
-  home: string
-}
+  maintenance: string;
+  home: string;
+  simulation: string;
+};
 
 type MaintenanceContent = {
-  title: string
-  description: string
-}
+  title: string;
+  description: string;
+};
 
 type HomeContent = {
-  title: string
-  description: string
-  imageURL: string
-  imageAlt: string
-  heroButtonLabel: string
-  heroTopics: string[]
-  simulationSectionTitle: string
-  simulationSectionDescription: string
-  simulationSectionTopics: string[]
-  simulationSectionMemberButtonLabel: string
-  simulationSectionMentorButtonLabel: string
-  podcastSectionTitle: string
-  podcastSectionDescription: string
-  popcornClubSectionTitle: string
-  popcornClubSectionDescription: string
-  popcornClubSectionButtonLabel: string
-  agileTrailSectionTitle: string
-  agileTrailSectionDescription: string
-  agileTrailSectionButtonLabel: string
-  popcornIndicationSectionTitle: string
-  popcornIndicationSectionDescription: string
-  popcornIndicationSectionButtonLabel: string
-  newsletterSectionTitle: string
-  newsletterSectionDescription: string
-  newsletterSectionButtonLabel: string
-}
+  title: string;
+  description: string;
+  imageURL: string;
+  imageAlt: string;
+  heroButtonLabel: string;
+  heroTopics: string[];
+  simulationSectionTitle: string;
+  simulationSectionDescription: string;
+  simulationSectionTopics: string[];
+  simulationSectionMemberButtonLabel: string;
+  simulationSectionMentorButtonLabel: string;
+  podcastSectionTitle: string;
+  podcastSectionDescription: string;
+  popcornClubSectionTitle: string;
+  popcornClubSectionDescription: string;
+  popcornClubSectionButtonLabel: string;
+  agileTrailSectionTitle: string;
+  agileTrailSectionDescription: string;
+  agileTrailSectionButtonLabel: string;
+  popcornIndicationSectionTitle: string;
+  popcornIndicationSectionDescription: string;
+  popcornIndicationSectionButtonLabel: string;
+  newsletterSectionTitle: string;
+  newsletterSectionDescription: string;
+  newsletterSectionButtonLabel: string;
+};
 
 type MenuOption = {
-  name: string
-  path: string
-  icon?: string
-  text: string
-  order: number
-}
+  name: string;
+  path: string;
+  icon?: string;
+  text: string;
+  order: number;
+};
 
 type Social = {
-  order: number
-  name: string
-  path: string
-  icon: ElementType
-}
+  order: number;
+  name: string;
+  path: string;
+  icon: ElementType;
+};
 
 type MaintenanceContentProps = {
-  pageName: string
-}
+  pageName: string;
+};
 
-type PageContainerProps = RootLayoutProps
+type PageContainerProps = RootLayoutProps;
+
+type SimulationContent = {
+  heroTitle: string;
+  heroSubtitle: string;
+  heroLabelButton: string;
+};
