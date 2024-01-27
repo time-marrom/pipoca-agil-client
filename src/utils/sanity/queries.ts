81f79fc92cb0e5b9f1002af48fd0b66e32eda68f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 23:54:22 -0300

    refactor: Update API base URL in axios.ts and queries.ts

--- src/utils/sanity/queries.ts
+++ src/utils/sanity/queries.ts
@@ -1,5 +1,5 @@
 export const queries: SanityQueries = {
-  simulation: "*%5B_type+%3D%3D+%22simulation%22%5D%5B0%5D",
-  home: "*%5B_type+%3D%3D+%22home%22%5D%5B0%5D",
-  maintenance: "*%5B_type+%3D%3D+%22maintenance%22%5D%5B0%5D"
+  simulation: "/production?query=*%5B_type+%3D%3D+%22simulation%22%5D%5B0%5D",
+  home: "/production?query=*%5B_type+%3D%3D+%22home%22%5D%5B0%5D",
+  maintenance: "/production?query=*%5B_type+%3D%3D+%22maintenance%22%5D%5B0%5D"
 }
