d82db54662796b8605131e5db17ecd59123bb75a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 22:50:35 -0300

    feat: Add axios service for fetching Sanity simulation content

---
+++ src/services/axios.ts
@@ -0,0 +1,15 @@
+import { queries } from "@/utils/sanity/queries";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://ejoo37fg.api.sanity.io/v2023-05-03/data/query/production",
+});
+
+export function getSanitySimulationContent(): Promise<SimulationContent> {
+  const response = api
+    .get(queries.simulation)
+    .then((response) => response.data.result)
+    .catch((error) => console.error(error));
+
+  return response;
+}
