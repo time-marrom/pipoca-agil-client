97ece6995020afc02730699bc475fa78e5d0ce66
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 23:01:13 -0300

    refactor: Update env.ts to parse process.env using envSchema

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,5 +6,5 @@ const envSchema = z.object({
   NEXT_PUBLIC_SANITY_API_VERSION: z.string(),
   NEXT_PUBLIC_SANITY_WEBHOOK_SECRET: z.string(),
 });
-console.log("process.env: ", process.env);
+
 export const env = envSchema.parse(process.env);
