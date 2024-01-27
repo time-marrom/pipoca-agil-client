457d6ea4c2717d3f398c4b674823c4511a77912c
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 23:28:02 -0300

    refactor: Update Sanity queries

--- src/utils/sanity/queries.ts
+++ src/utils/sanity/queries.ts
@@ -1,7 +1,5 @@
 export const queries: SanityQueries = {
-  simulation:
-    "v2023-05-03/data/query/production?query=*%5B_type+%3D%3D+%22simulation%22%5D%5B0%5D",
-  home: "v2023-05-03/data/query/production?query=*%5B_type+%3D%3D+%22home%22%5D%5B0%5D",
-  maintenance:
-    "v2023-05-03/data/query/production?query=*%5B_type+%3D%3D+%22maintenance%22%5D%5B0%5D"
+  simulation: "*%5B_type+%3D%3D+%22simulation%22%5D%5B0%5D",
+  home: "*%5B_type+%3D%3D+%22home%22%5D%5B0%5D",
+  maintenance: "*%5B_type+%3D%3D+%22maintenance%22%5D%5B0%5D"
 }
