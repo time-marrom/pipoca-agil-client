8c2845fbaf3076cfa984e81f7755faa68b54c95b
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-26 23:28:44 -0300

    refactor: Refactor MaintenanceContent component to use react-query and axios

--- src/components/MaintenanceContent/index.tsx
+++ src/components/MaintenanceContent/index.tsx
@@ -1,19 +1,22 @@
-import { client } from "@/utils/sanity/client"
-import { queries } from "@/utils/sanity/queries"
+import { getSanityMaintenanceContent } from "@/services/axios"
+import { useQuery } from "@tanstack/react-query"
 import Image from "next/image"
 
 export async function MaintenanceContent({
-  pageName
+  path,
+  content
 }: MaintenanceContentProps): Promise<JSX.Element> {
-  const maintenanceContent = await client.fetch<MaintenanceContent>(queries.maintenance)
+  const { data } = useQuery({
+    queryKey: ["maintenance"],
+    queryFn: getSanityMaintenanceContent,
+    initialData: content
+  })
 
   return (
     <div className="flex flex-col items-center justify-center text-center w-full min-h-[50vh] my-12 mx-auto gap-4">
-      <h1 className="text-2xl font-bold font-title">{maintenanceContent.title}</h1>
-      <span className="text-md font-base font-sans">
-        Página {pageName} em manutenção.
-      </span>
-      <p className="text-md font-base font-sans">{maintenanceContent.description}</p>
+      <h1 className="text-2xl font-bold font-title">{data.title}</h1>
+      <span className="text-md font-base font-sans">Página {path} em manutenção.</span>
+      <p className="text-md font-base font-sans">{data.description}</p>
       <Image
         src="https://media.tenor.com/NQYmifze-vQAAAAC/rihanna-work.gif"
         alt="Rihanna Work GIF"
