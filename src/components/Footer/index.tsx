8ebbabaa0bd130c4bd0a4fd3bbb8b098c0627908
Gabrielle177 <gabrielle.freitas177@gmail.com>
2024-01-29 19:18:33 -0300

    Footer em 1024

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Footer(): JSX.Element {
   const footerOptions = menuOptions.filter(
-    (option) => option.name !== "Início"
+    (option) => option.name !== "Quero mentorar"
   );
 
   const orderSocialMedias = socialMedias
@@ -12,37 +12,41 @@ export function Footer(): JSX.Element {
     .sort((a, b) => a.order - b.order);
 
   return (
-    <footer className="bg-[#F6F6F6] py-4 w-full h-full flex flex-col justify-between items-center gap-4 p-4">
-      <div className="w-full lg:w-[90vw] h-full flex flex-col lg:flex-row-reverse justify-between items-center gap-2 font-sans text-[#3A3A3A]">
-        <h4 className="block lg:hidden">Siga o Pipoca Ágil</h4>
-        <ul className="flex flex-row lg:flex-col gap-1 mb-1 ">
-          {orderSocialMedias.map(({ name, path, icon: Icon }) => (
-            <li key={name}>
-              <Link href={path} target="_blank">
-                <Icon className="h-6 w-6 fill-[#431B61]" />
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="w-full h-full flex flex-col justify-between items-center gap-2 my-4 py-2">
+    <footer className="bg-[#F6F6F6] py-4 px-36 w-full h-full flex flex-col justify-between items-center gap-8  ">
+      <div className="w-full h-full flex flex-col lg:flex-row-reverse justify-between items-start gap-2 font-sans text-[#3A3A3A]">
+        <div className="flex flex-col items-end mb-20 gap-8  ">
+          <h4 className="font-title font-semibold text-base text-[#3A3A3A] ">
+            Siga o Pipoca Ágil
+          </h4>
+          <ul className="flex flex-row gap-4  ">
+            {orderSocialMedias.map(({ name, path, icon: Icon }) => (
+              <li key={name}>
+                <Link href={path} target="_blank">
+                  <Icon className="h-8 w-8 fill-[#431B61] " />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-full h-full flex flex-col justify-between items-start gap-2 my-4 py-2">
           <p className="block lg:hidden my-2">Navegação</p>
-          <ul className="w-2/3 h-full flex flex-col lg:flex-row justify-between items-center gap-2 text-sm ">
+          <ul className="w-2/3 h-full flex flex-col lg:flex-row gap-20">
             {footerOptions.map(({ name, path, options }) => (
-              <li key={name} className="font-title font-semibold text-base ">
+              <li key={name} className="flex-row items-center">
                 <Link href={path}>
-                  <span className="hover:text-[#b667f3] underline">{name}</span>
+                  <span className="flex-row hover:text-[#b667f3] underline text-base font-semibold font-title">
+                    {name}
+                  </span>
                 </Link>
                 {options && options.length > 0 && (
                   <ul>
                     {options.map((option: string) => (
-                      <li key={option}>
-                        <Link
-                          href={`${path}/${option
-                            .toLowerCase()
-                            .replace(/\s+/g, "-")}`}
-                        >
-                          <span className=" font-sans">{option}</span>
-                        </Link>
+                      <li className="mb-8 mt-8 text-[12px]" key={option}>
+                        <span>
+                          <span className="flex flex-col font-sans text-sm font-normal">
+                            {option}
+                          </span>
+                        </span>
                       </li>
                     ))}
                   </ul>
@@ -52,12 +56,16 @@ export function Footer(): JSX.Element {
           </ul>
         </div>
       </div>
-      <hr className="w-full h-0.5 bg-[#431B61]" />
-      <div className="w-full h-full flex flex-col justify-between items-center gap-1 font-sans text-xs font-light text-[#3A3A3A] p-4">
-        <p>&copy;Time Marrom 2023. Todos os direitos reservados</p>
-        <p>Ibson Cabral</p>
-        <p>pipocaagil@gmail.com</p>
-        <p>Rio de Janeiro - RJ</p>
+      <hr className=" w-full h-0.5 bg-[#431B61]" />
+      <div className="w-full h-full flex flex-col justify-between items-center gap-4 font-sans text-xs font-light text-[#3A3A3A] px-4">
+        <p className="font-sans text-sm font-normal">
+          &copy;Time Marrom 2023. Todos os direitos reservados
+        </p>
+        <div className="w-full h-full flex flex-col justify-between items-center font-sans text-xs font-light text-[#3A3A3A]">
+          <p className="font-sans text-sm font-normal">Ibson Cabral</p>
+          <p className="font-sans text-sm font-normal">pipocaagil@gmail.com</p>
+          <p className="font-sans text-sm font-normal">Rio de Janeiro - RJ</p>
+        </div>
       </div>
     </footer>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,13 @@
 
 import React, { useCallback, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { menuOptions } from "@/utils/menu";
 import { socialMedias } from "@/utils/social";
 import { MenuIcon } from "../icons/MenuIcon";
 import { Button } from "../ui/button";
 import { Menu } from "./Menu";
+import icon from "@/assets/icon-popCorn.svg";
 
 interface HeaderProps {
   background: string;
@@ -28,34 +30,39 @@ export function Header({ background }: HeaderProps) {
     .sort((a, b) => a.order - b.order);
 
   const headerOptions = menuOptions.filter(
-    (option) => option.name !== "Clube do Pipoca"
+    (option) => option.name !== "Sobre os criadores"
   );
 
   return (
     <header
       className={`flex justify-between items-center text-sm py-3 px-5 ${background}`}
     >
-      <h1>Logo_do_pipoca.png</h1>
+      <Image src={icon} alt="icone de um microfone - pipoca ágil" />
       <nav className="hidden lg:flex items-center gap-10 text-md">
-        <ul className="flex flex-row gap-4">
+        <ul className="flex flex-row gap-12 text-base">
           {headerOptions.map(({ name, path }) => (
             <li key={name} className="font-sans font-semibold">
               <Link href={path}>{name}</Link>
             </li>
           ))}
+
+          <Button
+            className="border-2 border-[#5A0C94] text-[#5A0C94] rounded-2xl"
+            variant="outline"
+            asChild
+          >
+            <Link href="/clube-pipoca" className="font-title">
+              Quero voluntariar
+            </Link>
+          </Button>
         </ul>
       </nav>
       <div className="hidden md:flex flex-row justify-center items-center gap-10">
-        <Button variant="default" asChild>
-          <Link href="/clube-pipoca" className="font-sans">
-            Clube do Pipoca
-          </Link>
-        </Button>
         <ul className="flex flex-row gap-4">
           {orderSocialMedias.map(({ name, path, icon: Icon }) => (
             <li key={name}>
               <Link href={path} target="_blank">
-                <Icon className="h-6 w-6 fill-theme-grayscale-black" />
+                <Icon className="h-8 w-8 fill-theme-grayscale-black" />
               </Link>
             </li>
           ))}
