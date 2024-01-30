684ea92ba5e17c65f81db2971b1f33ca33b88353
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-29 23:07:43 -0300

    fix: Fix formatting issues in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,42 +1,34 @@
-"use client";
+"use client"
 
-import React, { useCallback, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { menuOptions } from "@/utils/menu";
-import { socialMedias } from "@/utils/social";
-import { MenuIcon } from "../icons/MenuIcon";
-import { Button } from "../ui/button";
-import { Menu } from "./Menu";
-import icon from "@/assets/icon-popCorn.svg";
+import icon from "@/assets/icon-popCorn.svg"
+import { menuOptions } from "@/utils/menu"
+import { socialMedias } from "@/utils/social"
+import Image from "next/image"
+import Link from "next/link"
+import { useCallback, useState } from "react"
+import { MenuIcon } from "../icons/MenuIcon"
+import { Button } from "../ui/button"
+import { Menu } from "./Menu"
 
-interface HeaderProps {
-  background: string;
-}
-
-export function Header({ background }: HeaderProps) {
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
+export function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
 
   const openMenu = useCallback(() => {
-    setMenuIsOpen(true);
-  }, []);
+    setMenuIsOpen(true)
+  }, [])
 
   const closeMenu = useCallback(() => {
-    setMenuIsOpen(false);
-  }, []);
+    setMenuIsOpen(false)
+  }, [])
 
-  const orderSocialMedias = socialMedias
-    .slice(0, 3)
-    .sort((a, b) => a.order - b.order);
+  const orderSocialMedias = socialMedias.slice(0, 3).sort((a, b) => a.order - b.order)
 
   const headerOptions = menuOptions.filter(
     (option) => option.name !== "Sobre os criadores"
-  );
+  )
 
   return (
-    <header
-      className={`flex justify-between items-center text-sm py-3 px-5 ${background}`}
-    >
+    <header className="flex justify-between items-center text-sm py-3 px-5">
       <Image src={icon} alt="icone de um microfone - pipoca ágil" />
       <nav className="hidden lg:flex items-center gap-10 text-md">
         <ul className="flex flex-row gap-12 text-base">
@@ -45,17 +37,16 @@ export function Header({ background }: HeaderProps) {
               <Link href={path}>{name}</Link>
             </li>
           ))}
-
-          <Button
-            className="border-2 border-[#5A0C94] text-[#5A0C94] rounded-2xl"
-            variant="outline"
-            asChild
-          >
-            <Link href="/clube-pipoca" className="font-title">
-              Quero voluntariar
-            </Link>
-          </Button>
         </ul>
+        <Button
+          className="border-2 border-[#5A0C94] text-[#5A0C94] rounded-2xl"
+          variant="outline"
+          asChild
+        >
+          <Link href="/clube-pipoca" className="font-title">
+            Quero voluntariar
+          </Link>
+        </Button>
       </nav>
       <div className="hidden md:flex flex-row justify-center items-center gap-10">
         <ul className="flex flex-row gap-4">
@@ -68,15 +59,10 @@ export function Header({ background }: HeaderProps) {
           ))}
         </ul>
       </div>
-      <Button
-        size="icon"
-        variant="ghost"
-        className="p-1 lg:hidden"
-        onClick={openMenu}
-      >
+      <Button size="icon" variant="ghost" className="p-1 lg:hidden" onClick={openMenu}>
         <MenuIcon className="h-6 w-6 fill-theme-grayscale-black" />
       </Button>
       <Menu isVisible={menuIsOpen} onClose={closeMenu} />
     </header>
-  );
+  )
 }
