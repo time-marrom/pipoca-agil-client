e1f8246d939d0bc36290427a3acf7872937d2228
Gabrielle177 <gabrielle.freitas177@gmail.com>
2024-01-30 19:40:14 -0300

    componente simulacao-descricao

--- src/app/simulacao/page.tsx
+++ src/app/simulacao/page.tsx
@@ -1,21 +1,21 @@
-import { PageContainer } from "@/components/PageContainer"
-import { SimulationCTA } from "@/components/simulation/SimulationCTA"
-import { SimulationDescription } from "@/components/simulation/SimulationDescription"
-import { SimulationFAQ } from "@/components/simulation/SimulationFAQ"
-import { SimulationHero } from "@/components/simulation/SimulationHero"
-import { SimulationTestimony } from "@/components/simulation/SimulationTestimony"
-import { getSanitySimulationContent } from "@/services/axios"
+import { PageContainer } from "@/components/PageContainer";
+import { SimulationCTA } from "@/components/simulation/SimulationCTA";
+import { SimulationDescription } from "@/components/simulation/SimulationDescription";
+import { SimulationFAQ } from "@/components/simulation/SimulationFAQ";
+import { SimulationHero } from "@/components/simulation/SimulationHero";
+import { SimulationTestimony } from "@/components/simulation/SimulationTestimony";
+import { getSanitySimulationContent } from "@/services/axios";
 
 export default async function SimulationPage() {
-  const content = await getSanitySimulationContent()
+  const content = await getSanitySimulationContent();
 
   return (
     <PageContainer>
       <SimulationHero content={content} />
-      <SimulationDescription />
+      <SimulationDescription content={content} />
       <SimulationCTA />
       <SimulationTestimony />
       <SimulationFAQ />
     </PageContainer>
-  )
+  );
 }
--- src/components/simulation/SimulationDescription/index.tsx
+++ src/components/simulation/SimulationDescription/index.tsx
@@ -1,25 +1,76 @@
-export function SimulationDescription() {
+"use client";
+
+import iconCheck from "@/assets/IconCheck.svg";
+import { CheckIcon } from "@/components/icons/CheckIcon";
+import { getSanitySimulationContent } from "@/services/axios";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+
+interface List {
+  text: string;
+  icon: string;
+  alt: string;
+}
+
+interface SimulationDescriptionProps {
+  content: SimulationContent;
+}
+
+export function SimulationDescription({ content }: SimulationDescriptionProps) {
+  const { data } = useQuery({
+    queryKey: ["simulation"],
+    queryFn: getSanitySimulationContent,
+    initialData: content,
+  });
+
+  const list: List[] = [
+    {
+      text: "Trabalhe em um grupo multidisciplinar",
+      icon: iconCheck,
+      alt: "icone de check",
+    },
+    {
+      text: "Participe de todo o processo - do problema à solução ",
+      icon: iconCheck,
+      alt: "icone de check",
+    },
+    {
+      text: "Aprenda agilidade na prática",
+      icon: iconCheck,
+      alt: "icone de check",
+    },
+    {
+      text: "Incremente seu currículo e portfólio",
+      icon: iconCheck,
+      alt: "icone de check",
+    },
+  ];
+
   return (
-    <div className="w-full h-full flex flex-col items-start justify-center text-start p-4 md:p-20 gap-2">
-      <h3 className="text-theme-grayscale-black text-2xl font-bold font-title mb-4">
-        O que é?
-      </h3>
-      <p className="text-theme-grayscale-darkest text-start font-sans mb-6 w-10/12">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-        voluptates, voluptatum, quibusdam, quia quae voluptatem quos
-        exercitationem quod nemo repellendus quas? Quisquam, voluptatibus
-        voluptatum. Quisquam, voluptatibus voluptatum. Quisquam, voluptatibus
-        voluptatum. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Dolorum doloremque, dolorem ut, neque quis debitis sint consequatur
-        maiores voluptate, nemo reiciendis dolores. Eos hic sequi rerum
-        laboriosam vero ab voluptatibus. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Ut sequi nisi recusandae? Sed necessitatibus id
-        veritatis cumque numquam, et atque dignissimos debitis libero sit
-        ratione quaerat ullam fugit natus obcaecati! Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Ad ipsum delectus itaque aut quis sapiente
-        corporis eaque! Distinctio repudiandae deserunt itaque quis, cupiditate
-        fuga accusamus iste eum molestias deleniti labore?
-      </p>
+    <div className="w-full h-full flex flex-col items-center justify-center text-center py-20 px-8 md:px-8 md:py-20 gap-2 rounded-t-[90px] lg:px-32 lg:py-40 bg-[#FCFCFC] ">
+      <div className="flex flex-col lg:flex-row lg:gap-12 w-full justify-between">
+        <div className="w-full lg:w-1/3 flex flex-col gap-8 lg:justify-center">
+          <h3 className="text-theme-grayscale-black text-4xl font-semibold font-title mb-4 text-center md:text-left">
+            {data.overviewPanelTitle}
+          </h3>
+          <p className="lg:w-3/4 font-sans font-normal text-lg mb-6 md:mb-10 lg:pr-4 text-center md:text-left">
+            {data.overviewPanelSubtitle}
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2 flex flex-col">
+          <ul className="w-full font-sans grid grid-flow-col grid-cols-1 grid-rows-4 lg:grid-cols-1 lg:grid-rows-4 md:grid-cols-2 md:grid-rows-2 ">
+            {data.overviewPanelTopics.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center text-lg mb-4 md:mb-6"
+              >
+                <CheckIcon className="fill-[#252525] hover:fill-purple-500 hover:scale-110 transition-all ease-in-out delay-100" />
+                <span className="ml-4">{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
