846b1718f8c929f7ed2ecdd9edb0caf8ce6f9eb9
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-05 01:46:17 -0300

    refactor: Atualiza o componente MaintenanceContent para exibir a página em manutenção com base no caminho atual.

--- src/components/MaintenanceContent/index.tsx
+++ src/components/MaintenanceContent/index.tsx
@@ -3,21 +3,22 @@
 import { getSanityMaintenanceContent } from "@/services/axios"
 import { useQuery } from "@tanstack/react-query"
 import Image from "next/image"
+import { usePathname } from "next/navigation"
 
-export function MaintenanceContent({
-  path,
-  content
-}: MaintenanceContentProps): JSX.Element {
+export function MaintenanceContent({ content }: MaintenanceContentProps): JSX.Element {
   const { data } = useQuery({
     queryKey: ["maintenance"],
     queryFn: getSanityMaintenanceContent,
     initialData: content
   })
+  const pathname = usePathname()
 
   return (
     <div className="flex flex-col items-center justify-center text-center w-full min-h-[50vh] my-12 mx-auto gap-4">
       <h1 className="text-2xl font-bold font-title">{data.title}</h1>
-      <span className="text-md font-base font-sans">Página {path} em manutenção.</span>
+      <span className="text-md font-base font-sans">
+        Página {pathname} em manutenção.
+      </span>
       <p className="text-md font-base font-sans">{data.description}</p>
       <Image
         src="https://media.tenor.com/NQYmifze-vQAAAAC/rihanna-work.gif"
