b98abe77ff0a22d6b1e407880a99b687a0b7e54f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-05 01:44:53 -0300

    refactor: Atualizar opções de menu

--- src/utils/menu.tsx
+++ src/utils/menu.tsx
@@ -1,27 +1,27 @@
 export const menuOptions: MenuOption[] = [
   {
     name: "Início",
-    options: ["Quero voluntariar", "Quero mentorar"],
     path: "/",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
-    order: 1,
+    order: 1
   },
   {
     name: "Emitir certificado",
-    path: "/simulacao",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
-    order: 2,
+    path: "/certificado",
+    order: 2
   },
   {
     name: "Sobre os criadores",
-    path: "/clube-pipoca",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
-    order: 3,
+    path: "/time-marrom",
+    order: 3
   },
   {
     name: "Quero mentorar",
-    path: "/podcast",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aspernatur eaque error, tempore voluptatum dignissimos? Hic sit cumque explicabo asperiores a ad rerum quis saepe est! Soluta, omnis eveniet. Harum.",
-    order: 4,
+    path: "/simulacao/inscricao",
+    order: 4
   },
-];
+  {
+    name: "Quero voluntariar",
+    path: "/simulacao/inscricao",
+    order: 5
+  }
+]
