d22abd57ae729af278c126de6886eb3357cf0ad6
emersonjesuz <josiemerson2013@gmail.com>
2024-02-21 08:56:56 -0300

    refector:0096 aplicando arredondamento na borda do componente

--- src/components/simulation/SimulationWhoIsItFor/index.tsx
+++ src/components/simulation/SimulationWhoIsItFor/index.tsx
@@ -32,69 +32,71 @@ export function SimulationWhoIsItFor({ content }: SimulationWhoIsItForProps) {
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center text-start py-20 px-8  md:px-8 md:py-20 gap- rounded-t-[90px] lg:px-32 lg:py-40 bg-[#FCFCFC] ">
-      <div className="w-full lg:w-1/2"></div>
-      <div className="w-full h-full  flex flex-col justify-center items-center gap-4 md:items-start ">
-        <div className="flex flex-col items-center justify-center gap-8 w-full ">
-          <h3 className="text-theme-grayscale-black text-4xl md:text-5xl font-semibold font-title mb-8 text-center ">
-            Posso me inscrever?
-          </h3>
-          <p className="text-theme-grayscale-darker font-sans text-[16px] md:text-[24px] text-center lg:w-[70%] ">
-            Você está estudando e/ou buscando emprego em alguma das áreas
-            abaixo? Sim? Então você está no lugar certo!
-          </p>
-        </div>
-        {isSmaller && (
-          <div className="w-full flex flex-col md:flex-row items-center justify-center my-10 gap-6">
-            <Image
-              src={whoIsItFor_1}
-              alt=""
-              className="flex items-center md:w-full md:h-full"
-            />
-            <Image
-              src={whoIsItFor_2}
-              alt=""
-              className="md:w-full md:h-1/7 md:mb-9 md:mr-5 "
-            />
-          </div>
-        )}
-        {!isSmaller && (
-          <div className="w-full flex flex-col md:flex-row md:items-center md:justify-center my-14">
-            <Image
-              src={whoIsItFor_lg}
-              alt=""
-              className="md:w-full md:h-1/7 md:mb-9 md:mr-5 "
-            />
-          </div>
-        )}
-        <div className="w-full flex flex-col items-center py-4 gap-12 md:gap-20 md:flex-row">
-          <div className=" flex flex-col items-center  md:items-start gap-4 md:gap-8 md:w-2/3">
-            <p className=" font-sans font-normal text-base mb-2 md:text-2xl  md:w-full">
-              {/* {data.volunteerText} */}
-              Se você está em transição de carreira para alguma das áreas acima
-              e quer finalmente ter aquela experiência prática, o projeto de
-              Simulação é para você.
+    <div className="bg-[#FCCF5C]">
+      <div className="w-full h-full flex flex-col items-center justify-center text-start py-20 px-8  md:px-8 md:py-20 gap- rounded-t-[90px]  lg:px-32 lg:py-40 bg-[#FCFCFC]">
+        <div className="w-full lg:w-1/2"></div>
+        <div className="w-full h-full  flex flex-col justify-center items-center gap-4 md:items-start ">
+          <div className="flex flex-col items-center justify-center gap-8 w-full ">
+            <h3 className="text-theme-grayscale-black text-4xl md:text-5xl font-semibold font-title mb-8 text-center ">
+              Posso me inscrever?
+            </h3>
+            <p className="text-theme-grayscale-darker font-sans text-[16px] md:text-[24px] text-center lg:w-[70%] ">
+              Você está estudando e/ou buscando emprego em alguma das áreas
+              abaixo? Sim? Então você está no lugar certo!
             </p>
-            <Link
-              href="/simulacao/inscricao"
-              className="flex h-12 max-w-max  p-4  text-base font-medium font-title rounded-2xl text-center items-center text-theme-white-base cursor-pointer transition duration-300 bg-theme-secondary-base hover:bg-theme-secondary-dark"
-            >
-              {data.volunteerLabelButton}
-            </Link>
           </div>
-          <div className="flex flex-col items-center md:items-start gap-4 md:gap-8 md:w-2/3">
-            <p className="font-sans font-normal text-base  mb-2 md:text-2xl md:w-full ">
-              {/* {data.mentorText} */}
-              Caso você já tenha uma bagagem sólida em alguma dessas áreas, pode
-              se juntar a nós para orientar e ajudar quem está dando seus
-              primeiros passos nos nossos times.
-            </p>
-            <Link
-              href="/simulacao/inscricao"
-              className="flex h-12 max-w-max  p-4 text-base font-medium font-title rounded-2xl text-center items-center text-theme-secondary-base cursor-pointer transition duration-300 bg-theme-white-base border-2 border-theme-secondary-base hover:bg-theme-white-light"
-            >
-              {data.mentorLabelButton}
-            </Link>
+          {isSmaller && (
+            <div className="w-full flex flex-col md:flex-row items-center justify-center my-10 gap-6">
+              <Image
+                src={whoIsItFor_1}
+                alt=""
+                className="flex items-center md:w-full md:h-full"
+              />
+              <Image
+                src={whoIsItFor_2}
+                alt=""
+                className="md:w-full md:h-1/7 md:mb-9 md:mr-5 "
+              />
+            </div>
+          )}
+          {!isSmaller && (
+            <div className="w-full flex flex-col md:flex-row md:items-center md:justify-center my-14">
+              <Image
+                src={whoIsItFor_lg}
+                alt=""
+                className="md:w-full md:h-1/7 md:mb-9 md:mr-5 "
+              />
+            </div>
+          )}
+          <div className="w-full flex flex-col items-center py-4 gap-12 md:gap-20 md:flex-row">
+            <div className=" flex flex-col items-center  md:items-start gap-4 md:gap-8 md:w-2/3">
+              <p className=" font-sans font-normal text-base mb-2 md:text-2xl  md:w-full">
+                {/* {data.volunteerText} */}
+                Se você está em transição de carreira para alguma das áreas
+                acima e quer finalmente ter aquela experiência prática, o
+                projeto de Simulação é para você.
+              </p>
+              <Link
+                href="/simulacao/inscricao"
+                className="flex h-12 max-w-max  p-4  text-base font-medium font-title rounded-2xl text-center items-center text-theme-white-base cursor-pointer transition duration-300 bg-theme-secondary-base hover:bg-theme-secondary-dark"
+              >
+                {data.volunteerLabelButton}
+              </Link>
+            </div>
+            <div className="flex flex-col items-center md:items-start gap-4 md:gap-8 md:w-2/3">
+              <p className="font-sans font-normal text-base  mb-2 md:text-2xl md:w-full ">
+                {/* {data.mentorText} */}
+                Caso você já tenha uma bagagem sólida em alguma dessas áreas,
+                pode se juntar a nós para orientar e ajudar quem está dando seus
+                primeiros passos nos nossos times.
+              </p>
+              <Link
+                href="/simulacao/inscricao"
+                className="flex h-12 max-w-max  p-4 text-base font-medium font-title rounded-2xl text-center items-center text-theme-secondary-base cursor-pointer transition duration-300 bg-theme-white-base border-2 border-theme-secondary-base hover:bg-theme-white-light"
+              >
+                {data.mentorLabelButton}
+              </Link>
+            </div>
           </div>
         </div>
       </div>
