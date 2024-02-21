5bb14d93bdaeacf9e9fe43189486e191eef1cb3f
Gabrielle177 <gabrielle.freitas177@gmail.com>
2024-02-21 01:34:40 -0300

    feat: creat page certificado

---
+++ src/app/certificado/page.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from "@/components/PageContainer";
+import { CertificateForm } from "@/components/certificate/CertificateForm";
+
+export default function Certificate(): JSX.Element {
+  return (
+    <PageContainer>
+      <CertificateForm />
+    </PageContainer>
+  );
+}
