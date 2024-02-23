ca44fd90e2cef5c82c8617c556a07076e5706c10
emersonjesuz <josiemerson2013@gmail.com>
2024-02-23 10:55:27 -0300

    refector: 0094 componente refatorado

--- src/components/simulation/SimulationDescription/index.tsx
+++ src/components/simulation/SimulationDescription/index.tsx
@@ -1,42 +1,54 @@
-"use client"
+"use client";
 
-import { CheckIcon } from "@/components/icons/CheckIcon"
-import { getSanitySimulationContent } from "@/services/axios"
-import { useQuery } from "@tanstack/react-query"
+import { CheckIcon } from "@/components/icons/CheckIcon";
+import { SimulationScrunIcon } from "@/components/icons/SimulationScrumSvg";
+import { getSanitySimulationContent } from "@/services/axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface SimulationDescriptionProps {
-  content: SimulationContent
+  content: SimulationContent;
 }
 
 export function SimulationDescription({ content }: SimulationDescriptionProps) {
   const { data } = useQuery({
     queryKey: ["simulation"],
     queryFn: getSanitySimulationContent,
-    initialData: content
-  })
+    initialData: content,
+  });
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center text-start py-20 px-8 md:px-8 md:py-20 gap-2 rounded-t-[90px] lg:px-32 lg:py-40 bg-[#FCFCFC]">
-      <div className="flex flex-col lg:flex-row lg:gap-12 md:gap-12 w-full justify-between ">
-        <div className="w-full lg:w-1/3 flex flex-col gap-8 lg:justify-center ">
-          <h3 className="text-theme-grayscale-black text-4xl font-semibold font-title mb-4 text-center md:text-left">
-            {data.overviewPanelTitle}
-          </h3>
-          <p className="lg:w-3/4 font-sans font-normal text-base mb-6 md:mb-10 lg:pr-4 text-start md:text-left">
-            {data.overviewPanelSubtitle}
-          </p>
-        </div>
-        <div className="w-full lg:w-1/2 flex flex-col">
-          <ul className="w-full font-sans grid grid-flow-col grid-cols-1 grid-rows-4 lg:grid-cols-1 lg:grid-rows-4 md:grid-cols-2 md:grid-rows-2 ml:pr-8 ">
-            {data.overviewPanelTopics.map((item, index) => (
-              <li key={index} className="flex items-center text-base mb-4 md:mb-6">
-                <CheckIcon className="fill-[#252525] hover:fill-purple-500 hover:scale-110 transition-all ease-in-out delay-100" />
-                <span className="ml-4 w-full">{item}</span>
-              </li>
-            ))}
-          </ul>
+    <div className="bg-[#FCCF5C] w-full">
+      <div className="w-full h-full flex flex-col items-center justify-center text-start px-4 md:px-36  py-12 md:py-[104px]  gap-2 rounded-t-[90px] lg:px-32 lg:py-40 bg-[#FCFCFC]">
+        <div className="flex flex-col-reverse lg:flex-row lg:gap-12 md:gap-12 w-full justify-between ">
+          <div className="w-full  flex flex-col justify-center items-center lg:items-start gap-10 ">
+            <h3 className="text-theme-grayscale-black  block lg:hidden text-4xl  font-semibold font-title text-center md:text-left">
+              {data.overviewPanelTitle}
+            </h3>
+
+            <div className="flex flex-col-reverse lg:flex-row gap-10 lg:w-full">
+              <div className="flex gap-4 flex-col lg:justify-center w-full">
+                <h3 className="hidden lg:block text-theme-grayscale-black text-4xl lg:text-5xl font-semibold font-title text-center md:text-left">
+                  {data.overviewPanelTitle}
+                </h3>
+                <p className=" font-sans font-normal text-base lg:text-2xl  text-start md:text-left">
+                  A Simulação de Projetos Ágeis, iniciativa do podcast Pipoca
+                  Ágil, tem o objetivo de proporcionar uma experiência prática
+                  para quem está em transição de carreira para a área de
+                  tecnologia.
+                </p>
+                <p className="font-sans font-normal text-base lg:text-2xl  text-start md:text-left">
+                  A proposta é simular o máximo possível a rotina dentro de uma
+                  empresa, utilizando o Scrum para o desenvolvimento de um
+                  produto digital.
+                </p>
+              </div>
+              <div className="w-full flex flex-col  justify-center items-center">
+                <SimulationScrunIcon className="flex  w-[328px] h-[292px] lg:w-full lg:h-[492px]" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
