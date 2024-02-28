e95af482fda1bf8cf5094135a90efcd1333c3c73
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-27 23:19:26 -0300

    refactor: Add page container for NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
 import { MaintenanceContent } from "@/components/MaintenanceContent"
+import { PageContainer } from "@/components/PageContainer"
 import { getSanityMaintenanceContent } from "@/services/axios"
 
 export default async function NotFound() {
   const content = await getSanityMaintenanceContent()
-  return <MaintenanceContent content={content} />
+  return (
+    <PageContainer>
+      <MaintenanceContent content={content} />
+    </PageContainer>
+  )
 }
