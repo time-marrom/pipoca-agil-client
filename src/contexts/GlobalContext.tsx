9de7d56974d928ce7b517609a34342dfc8ad50bc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-04 04:37:55 -0300

    refactor: Update GlobalContext.tsx

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,31 +1,33 @@
-import * as React from "react";
+import * as React from "react"
 
 type GlobalContextProps = {
-  isAcceptedTerms: boolean;
-  setIsAcceptedTerms: React.Dispatch<React.SetStateAction<boolean>>;
-  role: string;
-  setRole: React.Dispatch<React.SetStateAction<string>>;
-};
+  isAcceptedTerms: boolean
+  setIsAcceptedTerms: React.Dispatch<React.SetStateAction<boolean>>
+  role: string
+  setRole: React.Dispatch<React.SetStateAction<string>>
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>
+  currentStep: number
+}
 
 type GlobalContextProviderProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const DEFAULT_VALUE = {
   isAcceptedTerms: false,
   setIsAcceptedTerms: () => {},
   role: "",
   setRole: () => {},
-};
+  currentStep: 0,
+  setCurrentStep: () => {}
+}
 
-export const GlobalContext =
-  React.createContext<GlobalContextProps>(DEFAULT_VALUE);
+export const GlobalContext = React.createContext<GlobalContextProps>(DEFAULT_VALUE)
 
-export function GlobalContextProvider({
-  children,
-}: GlobalContextProviderProps) {
-  const [isAcceptedTerms, setIsAcceptedTerms] = React.useState<boolean>(false);
-  const [role, setRole] = React.useState<string>("VOLUNTÁRIO");
+export function GlobalContextProvider({ children }: GlobalContextProviderProps) {
+  const [isAcceptedTerms, setIsAcceptedTerms] = React.useState<boolean>(false)
+  const [role, setRole] = React.useState<string>("VOLUNTÁRIO")
+  const [currentStep, setCurrentStep] = React.useState<number>(0)
 
   const memoizedValue = React.useMemo(
     () => ({
@@ -33,13 +35,11 @@ export function GlobalContextProvider({
       setIsAcceptedTerms,
       role,
       setRole,
+      currentStep,
+      setCurrentStep
     }),
-    [isAcceptedTerms, setIsAcceptedTerms, role, setRole]
-  );
+    [isAcceptedTerms, setIsAcceptedTerms, role, setRole, currentStep, setCurrentStep]
+  )
 
-  return (
-    <GlobalContext.Provider value={memoizedValue}>
-      {children}
-    </GlobalContext.Provider>
-  );
+  return <GlobalContext.Provider value={memoizedValue}>{children}</GlobalContext.Provider>
 }
