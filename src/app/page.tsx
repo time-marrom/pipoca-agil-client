09de729d749db7d77fba159e959c1315e4e0d7fb
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-08 18:16:27 -0300

    feat: Add testimonials and FAQ components to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { PageContainer } from "@/components/PageContainer"
+import { Depositions } from "@/components/simulation/Depositions"
 import { SimulationDescription } from "@/components/simulation/SimulationDescription"
+import { SimulationFAQ } from "@/components/simulation/SimulationFAQ"
 import { SimulationHero } from "@/components/simulation/SimulationHero"
 import { SimulationHowItWorks } from "@/components/simulation/SimulationHowItWorks"
 import { SimulationWhoIsItFor } from "@/components/simulation/SimulationWhoIsItFor"
@@ -16,6 +18,8 @@ export default async function Home() {
       <SimulationHowItWorks content={content} />
       <SimulationWhoIsItFor content={content} />
       <SimulationWorthIt content={content} />
+      <Depositions />
+      <SimulationFAQ />
     </PageContainer>
   )
 }
