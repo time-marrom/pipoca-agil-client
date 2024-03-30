317b60f870591a6a05158656091f6b6004715f33
Gabrielle177 <gabrielle.freitas177@gmail.com>
2024-03-30 15:02:44 -0300

    fix: corrigir hero

--- src/components/Creators/HeroCreators/index.tsx
+++ src/components/Creators/HeroCreators/index.tsx
@@ -20,13 +20,13 @@ export function HeroCreators({ content }: HeroCreatorsProps) {
 
   return (
     <div className="w-full h-full  flex flex-col lg:flex-col justify-center px-8 pt-20 md:pt-40 md:pb-20 items-center bg-[#FCCF5C]">
-      <div className="flex flex-col md:flex-row md:justify-center md:items-center lg:items-start gap-2 bg-purple-400">
+      <div className="flex flex-col md:flex-row md:justify-center md:items-center lg:items-start gap-2 ">
         <Image
-          className="h-[250px] w-[300px] md:h-[280px] md:w-[320px] lg:h-[450px] lg:w-[600px] md:block hidden bg-red-500"
+          className="h-[250px] w-[300px] md:h-[280px] md:w-[320px] lg:h-[450px] lg:w-[600px] md:block hidden"
           src={ibson}
           alt="Montagem com a foto de um homem que sorri. Ele possui cabelo curto e grisalho, barba, usa óculos preto e camiseta cinza, e está de braços cruzados. Ao fundo, em volta da foto, está o símbolo do framework Scrum"
         />
-        <div className=" h-full w-full lg:w-1/2 flex flex-col md:flex-row lg:flex-col justify-between items-start bg-pink-400">
+        <div className=" h-full w-full lg:w-1/2 flex flex-col md:flex-row lg:flex-col justify-between items-start ">
           <div>
             <h3 className=" text-theme-grayscale-black text-[32px]  md:text-5xl lg:text-[64px] font-medium font-title leading-[62px] xl:text-start mb-4 lg:mb-6">
               {data.creatorsHeroTitle}
@@ -59,10 +59,10 @@ export function HeroCreators({ content }: HeroCreatorsProps) {
           </div>
         </div>
       </div>
-      <div className=" flex-col justify-start items-start text-start font-sans text-base font-normal md:block lg:hidden w-full max-w-[625px] bg-orange-300">
+      <div className=" flex-col justify-start items-start text-start font-sans text-base font-normal md:block lg:hidden w-full md:mt-6">
         <p className="mb-6 ">{data.creatorsHeroFirstParagraph}</p>
         <p>{data.creatorsHeroSecondParagraph}</p>
-        <div className="flex flex-row text-start items-start my-10 bg-red-500">
+        <div className="flex flex-row text-start items-start my-10">
           <Image src={iconLinkedin} alt="icone linkedin" />
           <Link
             href="https://www.linkedin.com/in/ibson-cabral"
