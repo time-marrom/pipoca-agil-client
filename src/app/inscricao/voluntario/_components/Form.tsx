3d8e67ec1b2a88b936d14efcbaefb0fc2e1e88cd
Gabrielle177 <gabrielle.freitas177@gmail.com>
2024-04-02 00:17:38 -0300

    fix:Implementar icone no input quando error

--- src/app/inscricao/voluntario/_components/Form.tsx
+++ src/app/inscricao/voluntario/_components/Form.tsx
@@ -7,6 +7,7 @@ import * as z from "zod";
 import { WarningIcon } from "@/components/icons/WarningIcon";
 import { sendVolunteerConfirmation } from "@/services/email";
 import Link from "next/link";
+import { Error } from "@/components/icons/Error";
 
 interface FormProps {
   setCurrentStep: (currentStep: number) => void;
@@ -98,14 +99,19 @@ export function Form({ setCurrentStep, currentStep }: FormProps) {
                 >
                   *Nome Completo
                 </label>
-                <input
-                  type="text"
-                  data-error={errors.name}
-                  data-filled={!!watch("name")}
-                  placeholder="Nome e sobrenome"
-                  className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000] "
-                  {...register("name")}
-                />
+                <div className="relative">
+                  <input
+                    type="text"
+                    data-error={errors.name}
+                    data-filled={!!watch("name")}
+                    placeholder="Nome e sobrenome"
+                    className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000] "
+                    {...register("name")}
+                  />
+                  {errors.name && (
+                    <Error className="w-6 h-6 absolute bottom-2  right-4" />
+                  )}
+                </div>
                 <p
                   data-error={errors.name}
                   className="w-4/5 text-sm font-medium font-sans leading-[35px] text-black data-[error]:text-[#e70000]"
@@ -131,14 +137,19 @@ export function Form({ setCurrentStep, currentStep }: FormProps) {
                 >
                   *E-mail para contato
                 </label>
-                <input
-                  type="email"
-                  data-error={errors.email}
-                  data-filled={!!watch("email")}
-                  placeholder="exemplo@exemplo.com"
-                  className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
-                  {...register("email")}
-                />
+                <div className="relative">
+                  <input
+                    type="email"
+                    data-error={errors.email}
+                    data-filled={!!watch("email")}
+                    placeholder="exemplo@exemplo.com"
+                    className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
+                    {...register("email")}
+                  />
+                  {errors.email && (
+                    <Error className="w-6 h-6 absolute bottom-2 right-4" />
+                  )}
+                </div>
                 <p
                   data-error={errors.email}
                   className="w-4/5 text-sm font-medium font-sans leading-[35px] text-black data-[error]:text-[#e70000]"
@@ -170,14 +181,19 @@ export function Form({ setCurrentStep, currentStep }: FormProps) {
                 >
                   *Número de WhatsApp
                 </label>
-                <input
-                  type="text"
-                  data-error={errors.whatsApp}
-                  data-filled={!!watch("whatsApp")}
-                  placeholder="(99) 99 9 9999-9999"
-                  className="w-full h-10 px-4 text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
-                  {...register("whatsApp")}
-                />
+                <div className="relative">
+                  <input
+                    type="text"
+                    data-error={errors.whatsApp}
+                    data-filled={!!watch("whatsApp")}
+                    placeholder="(99) 99 9 9999-9999"
+                    className="w-full h-10 px-4 text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
+                    {...register("whatsApp")}
+                  />
+                  {errors.whatsApp && (
+                    <Error className="w-6 h-6 absolute bottom-2 right-4" />
+                  )}
+                </div>
                 <p
                   data-error={errors.whatsApp}
                   className="w-4/5 text-sm font-medium font-sans leading-[35px] text-black data-[error]:text-[#e70000]"
@@ -358,14 +374,19 @@ export function Form({ setCurrentStep, currentStep }: FormProps) {
                 >
                   *Perfil do LinkedIn
                 </label>
-                <input
-                  type="text"
-                  data-error={errors.linkedIn}
-                  data-filled={!!watch("linkedIn")}
-                  placeholder="https://linkedin.com/in/seulink"
-                  className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
-                  {...register("linkedIn")}
-                />
+                <div className="relative">
+                  <input
+                    type="text"
+                    data-error={errors.linkedIn}
+                    data-filled={!!watch("linkedIn")}
+                    placeholder="https://linkedin.com/in/seulink"
+                    className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
+                    {...register("linkedIn")}
+                  />
+                  {errors.linkedIn && (
+                    <Error className="w-6 h-6 absolute bottom-2 right-4" />
+                  )}
+                </div>
 
                 <p
                   data-error={errors.linkedIn}
@@ -392,14 +413,19 @@ export function Form({ setCurrentStep, currentStep }: FormProps) {
                 >
                   *Tem alguma observação a fazer?
                 </label>
-                <input
-                  type="textarea"
-                  data-error={errors.comment}
-                  data-filled={!!watch("comment")}
-                  placeholder="Conte pra gente!"
-                  className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
-                  {...register("comment")}
-                />
+                <div className="relative">
+                  <input
+                    type="textarea"
+                    data-error={errors.comment}
+                    data-filled={!!watch("comment")}
+                    placeholder="Conte pra gente!"
+                    className="w-full h-10 px-4 text-[15px] text-base font-medium font-sans placeholder:text-[#dedede] border border-[#c3c3c3] data-[filled=true]:border-[#5a0c94]  rounded-md focus:outline-none focus:ring-2 focus:ring-[#5a0c94] hover:border-[#5a0c94] focus:border-transparent data-[error]:border-[#e70000] data-[error]:text-[#e70000] data-[error]:focus:border-[#e70000] data-[error]:focus:ring-[#e70000] data-[error]:hover:border-[#e70000] data-[error]:placeholder:text-[#e70000]"
+                    {...register("comment")}
+                  />
+                  {errors.comment && (
+                    <Error className="w-6 h-6 absolute bottom-2 right-4" />
+                  )}
+                </div>
                 <p
                   data-error={errors.comment}
                   className="w-4/5 text-sm font-medium font-sans leading-[35px] text-black data-[error]:text-[#e70000]"
