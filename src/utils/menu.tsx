7caa3576402b55f028121785d4463db847471fcc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-04-03 12:14:26 -0300

    Atualizar ordem dos itens do menu

--- src/utils/menu.tsx
+++ src/utils/menu.tsx
@@ -2,31 +2,31 @@ export const menuOptions: MenuOption[] = [
   {
     name: "Início",
     path: "/",
-    order: 1,
+    order: 1
   },
   {
     name: "Sobre os criadores",
     path: "/criadores",
-    order: 2,
+    order: 2
   },
   {
     name: "Emitir certificado",
     path: "/inscricao/certificado",
-    order: 3,
+    order: 3
   },
   {
     name: "Fale com a gente",
     path: "/contato",
-    order: 4,
+    order: 4
   },
   {
     name: "Quero voluntariar",
     path: "/inscricao/voluntario",
-    order: 5,
+    order: 5
   },
   {
     name: "Quero mentorar",
-    path: "/inscricao/mentorar",
-    order: 6,
-  },
-];
+    path: "/inscricao/mentor",
+    order: 6
+  }
+]
