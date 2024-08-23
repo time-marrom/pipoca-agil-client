a74b8023199c1b734fc7b709bce2b96e61fc618b
emersonjesuz <josiemerson2013@gmail.com>
2024-08-23 09:35:08 -0300

    feat: remove forms

--- src/app/inscricao/mentor/page.tsx
+++ src/app/inscricao/mentor/page.tsx
@@ -1,12 +1,28 @@
 import { MentorPanel } from "@/components/registration/MentorPanel";
 import { RegistrationContainer } from "@/components/registration/RegistrationContainer";
+import { Button } from "@/components/ui/button";
 import { getSanityMentorContent } from "@/services/axios";
+import Link from "next/link";
 
 export default async function MentorRegistration() {
   const content = await getSanityMentorContent();
   return (
     <RegistrationContainer>
-      <MentorPanel content={content} />
+      {/* <MentorPanel content={content} /> */}
+      <div className="w-full h-full min-h-[65vh] flex flex-col justify-start items-center space-y-8">
+        <h1 className="font-title text-3xl">Inscrições Encerradas</h1>
+        <p className="font-sans text-base ">
+          A inscricão para o mentor foi encerrada. Acompanhe o Pipoca Ágil nas
+          redes sociais para saber as novidades do projeto.
+        </p>
+        <div className="flex justify-center">
+          <Link href="/">
+            <Button className="rounded-xl font-title font-medium text-base">
+              Voltar para o Início
+            </Button>
+          </Link>
+        </div>
+      </div>
     </RegistrationContainer>
   );
 }
--- src/app/inscricao/voluntario/page.tsx
+++ src/app/inscricao/voluntario/page.tsx
@@ -1,13 +1,29 @@
 import { RegistrationContainer } from "@/components/registration/RegistrationContainer";
 import { RegistrationPanel } from "@/components/registration/RegistrationPanel";
+import { Button } from "@/components/ui/button";
 import { getSanityVoluntaryContent } from "@/services/axios";
+import Link from "next/link";
 
 export default async function VolunteerRegistration() {
   const content = await getSanityVoluntaryContent();
 
   return (
     <RegistrationContainer>
-      <RegistrationPanel content={content} />
+      {/* <RegistrationPanel content={content} /> */}
+      <div className="w-full h-full min-h-[65vh] flex flex-col justify-start items-center space-y-8">
+        <h1 className="font-title text-3xl">Inscrições Encerradas</h1>
+        <p className="font-sans text-base ">
+          A inscricão para o voluntário foi encerrada. Acompanhe o Pipoca Ágil
+          nas redes sociais para saber as novidades do projeto.
+        </p>
+        <div className="flex justify-center">
+          <Link href="/">
+            <Button className="rounded-xl font-title font-medium text-base">
+              Voltar para o Início
+            </Button>
+          </Link>
+        </div>
+      </div>
     </RegistrationContainer>
   );
 }
